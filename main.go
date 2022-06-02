df94fb7e2056f9e9a0b60bcf1387d3291b96b697
Alex Y. Chan <ayb.chan@gmail.com>
2022-06-02 19:50:30 +0100

    Add bye endpoint

--- main.go
+++ main.go
@@ -9,19 +9,15 @@ import (
 )
 
 func main() {
-	// create handler
+	// create handlers
 	l := log.New(os.Stdout, "api", log.LstdFlags)
 	helloHandler := handlers.NewHello(l)
+	byeHandler := handlers.NewBye(l)
 
-	// register handler to servemux
+	// register handlers to servemux
 	sm := http.NewServeMux()
-	sm.Handle("/", helloHandler)
-
-	//http.HandleFunc("/bye", func(rw http.ResponseWriter, r *http.Request) {
-	//	log.Println("good bye")
-
-	//	os.Exit(3)
-	//})
+	sm.Handle("/hello", helloHandler)
+	sm.Handle("/bye", byeHandler)
 
 	http.ListenAndServe(":9090", sm)
 }
