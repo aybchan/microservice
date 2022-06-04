0cc007ccd18b494c4a5d40e9558edb1bbf8af272
Alex Y. Chan <ayb.chan@gmail.com>
2022-06-04 02:14:35 +0100

    Add struct field json tags

--- data/products.go
+++ data/products.go
@@ -3,13 +3,13 @@ package data
 import "time"
 
 type Product struct {
-	ID          int32
-	Name        string
-	Description string
-	Price       float32
-	SKU         string
-	CreatedAt   string
-	UpdatedAt   string
+	ID          int32   `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedAt   string  `json:"-"`
+	UpdatedAt   string  `json:"-"`
 }
 
 func GetProducts() []*Product {
