78d18b4d155332e5b53c5cceef9e3f094cafe569
Alex Y. Chan <ayb.chan@gmail.com>
2022-06-04 01:53:52 +0100

    Add coffee

---
+++ data/products.go
@@ -0,0 +1,34 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID      string
+	Name    string
+	Description string
+	Price float32
+	SKU string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+var ProductList = []*Product{
+	&Product{
+		ID: 0,
+		Name: "Espresso",
+		Description: "Short coffee",
+		Price: "1.00",
+		SKU: "coffee00",
+		CreatedAt: time.Now().UTC().String(),
+		UpdatedAt: time.Now().UTC().String(),
+	},
+	&Product{
+		ID: 1,
+		Name: "Latte",
+		Description: "Foamy milk and espresso",
+		Price: "2.45",
+		SKU: "coffee01",
+		CreatedAt: time.Now().UTC().String(),
+		UpdatedAt: time.Now().UTC().String(),
+	}
+}
